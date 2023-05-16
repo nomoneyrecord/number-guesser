bc9d4e2cf597dde5cd5268219f6d0d990b2c1d6d
Gary H <3rdeye.ghj@gmail.com>
2023-05-16 15:39:14 -0700

    Add all functions to make game work

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
 
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+  const humanDifference = Math.abs(humanGuess - targetNumber);
+  const computerDifference = Math.abs(computerGuess - targetNumber);
+
+  return humanDifference <= computerDifference; 
+  }
+
+const updateScore = (winner) => winner === 'human' ? humanScore++ : computerScore++; 
+
+
+const advanceRound = () => computerScore >= humanScore || humanScore >= computerScore ? currentRoundNumber++ : 0; 
