89884413a87a0fd6e0dfb99d15631081327ba9ad
Gary H <3rdeye.ghj@gmail.com>
2023-05-16 16:02:21 -0700

    Added alert feature for when user types in number out of game set range

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ const generateTarget = () => {
   return Math.floor(Math.random() * 10);
 }
 
+const isGuessValid = (guess) => {
+  return guess >= 0 && guess <= 10; 
+}
+
 const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+  if (!isGuessValid(humanGuess)) {
+    alert('Your guess is out of range! Please enter a number between 0 and 10.');
+    return false;
+  }
   const humanDifference = Math.abs(humanGuess - targetNumber);
   const computerDifference = Math.abs(computerGuess - targetNumber);
 
