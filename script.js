16d0e60f5eb7407a81e9326650a80363ec360ff0
Gary H <3rdeye.ghj@gmail.com>
2023-05-16 16:12:49 -0700

    refactored line 28 to be more elegant and efficient

--- script.js
+++ script.js
@@ -2,29 +2,27 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-
 const generateTarget = () => {
   return Math.floor(Math.random() * 10);
-}
+};
 
 const isGuessValid = (guess) => {
-  return guess >= 0 && guess <= 10; 
-}
+  return guess >= 0 && guess <= 10;
+};
 
 const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
   if (!isGuessValid(humanGuess)) {
-    alert('Your guess is out of range! Please enter a number between 0 and 10.');
+    alert(
+      "Your guess is out of range! Please enter a number between 0 and 10."
+    );
     return false;
   }
   const humanDifference = Math.abs(humanGuess - targetNumber);
   const computerDifference = Math.abs(computerGuess - targetNumber);
+  return humanDifference <= computerDifference;
+};
 
-  return humanDifference <= computerDifference; 
-  }
+const updateScore = (winner) =>
+  winner === "human" ? humanScore++ : computerScore++;
 
-const updateScore = (winner) => winner === 'human' ? humanScore++ : computerScore++; 
-
-
-const advanceRound = () => computerScore >= humanScore || humanScore >= computerScore ? currentRoundNumber++ : 0; 
-
-
+const advanceRound = () => currentRoundNumber++;
